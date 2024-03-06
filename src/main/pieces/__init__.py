3d0aaf1324aa99f74b3841cf63227f7231869bf7
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-06 15:13:09 +0000

    Create __init__.py

---
+++ src/main/pieces/__init__.py
@@ -0,0 +1,6 @@
+# chess_game/__init__.py
+# This file can be left empty or used for package-wide imports and variables.
+
+# chess_game/pieces/__init__.py
+# This file can also be left empty or used for specific imports from the
+# pieces package.
