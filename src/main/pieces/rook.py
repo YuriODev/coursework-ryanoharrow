bc47287a3db6b981595aa7efa7b455f671eee038
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-06 17:14:00 +0000

    Update rook.py

--- src/main/pieces/rook.py
+++ src/main/pieces/rook.py
@@ -6,9 +6,8 @@ class Rook(Piece):
     """
     Represents a rook in the chess game. Inherits from Piece.
 
-    The Rook moves straight forward one square, or optionally, two squares
-    from its starting position, but it captures diagonally. Rooks can also
-    perform a special move known as "en passant."
+    The Rook moves any number of squares horizontally or vertically, forward
+    or backward, through any number of unoccupied squares.
     """
 
     def __init__(self, color: str, position: tuple, board: list):
@@ -27,38 +26,8 @@ class Rook(Piece):
         if not self.is_move_within_bounds(new_position):
             return False
 
-        # Check if the move is valid
-        if self._is_vertical_move(new_position) or \
-                self._is_horizontal_move(new_position):
-            return self._is_empty(new_position) or \
+        # Check if the move is horizontal or vertical and the path is clear
+        if self._is_horizontal_move(new_position) or \
+                self._is_vertical_move(new_position):
+            return self._is_empty_path(new_position) or \
                 self._is_opposite_color(new_position)
-
-        return False
-
-    def _is_vertical_move(self, new_position: tuple) -> bool:
-        """
-        Checks if the move is a vertical move.
-
-        Args:
-            new_position (tuple): The proposed new position for the rook.
-
-        Returns:
-            bool: True if the move is a vertical move, False otherwise.
-        """
-        row, col = self.position
-        new_row, new_col = new_position
-        return new_col == col and new_row != row
-
-    def _is_horizontal_move(self, new_position: tuple) -> bool:
-        """
-        Checks if the move is a horizontal move.
-
-        Args:
-            new_position (tuple): The proposed new position for the rook.
-
-        Returns:
-            bool: True if the move is a horizontal move, False otherwise.
-        """
-        row, col = self.position
-        new_row, new_col = new_position
-        return new_row == row and new_col != col
