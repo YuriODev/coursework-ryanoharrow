bd1522f16381dab76ce66de7ffd167505d70fc69
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-10 22:50:07 +0000

    Update rook.py

--- src/main/pieces/rook.py
+++ src/main/pieces/rook.py
@@ -1,30 +1,37 @@
 # chess_game/pieces/rook.py
+from __future__ import annotations
 from .piece import Piece
+from .validator import MoveValidator
 
 
 class Rook(Piece):
     """
-    Represents a rook in the chess game. Inherits from Piece.
+    Represents a rooke in the chess game. Inherits from Piece.
 
-    The Rook moves any number of squares horizontally or vertically, forward
-    or backward, through any number of unoccupied squares.
+    The Rooke moves any number of squares horizontally or vertically.
     """
 
-    def __init__(self, color: str, position: tuple, board: list):
-        super().__init__(color, "Rook", position, board)
-
-    def is_valid_move(self, new_position: tuple) -> bool:
+    def __init__(self, colour: str, position: tuple):
         """
-        Checks if a move is valid for the rook.
+        Initializes a new Rooke. Inherits from Piece.
 
         Args:
-            new_position (tuple): The colour new position for the rook.
+            colour (str): The colour of the rooke.
+            position (tuple): The starting position of the rooke.
+        """
+        super().__init__(colour, "Rook", position)
+
+    def is_valid_move(self, new_position: tuple,
+                      chess_board: ChessBoard) -> bool:
+        """
+        Checks if a move is valid for the rooke.
+
+        Args:
+            new_position (tuple): The proposed new position for the rooke.
+            chess_board (ChessBoard): The current state of the board.
 
         Returns:
             bool: True if the move is valid, False otherwise.
         """
-        if not self.is_move_within_bounds(new_position):
-            return False
-
-        # Check if the move is horizontal or vertical and the path is clear
-        return self._is_rook_move_valid(new_position)
+        return MoveValidator.is_rook_move_valid(self.position, new_position,
+                                                chess_board)
