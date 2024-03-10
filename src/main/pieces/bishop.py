27d75312e9fbf07b4a83129d1d07a7fa97502ee3
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-10 22:50:20 +0000

    Update bishop.py

--- src/main/pieces/bishop.py
+++ src/main/pieces/bishop.py
@@ -1,5 +1,7 @@
 # chess_game/pieces/bishop.py
+from __future__ import annotations
 from .piece import Piece
+from .validator import MoveValidator
 
 
 class Bishop(Piece):
@@ -9,21 +11,27 @@ class Bishop(Piece):
     The Bishop moves any number of squares diagonally.
     """
 
-    def __init__(self, colour: str, position: tuple, board: list):
-        super().__init__(colour, "Bishop", position, board)
+    def __init__(self, colour: str, position: tuple):
+        """
+        Initializes a new Bishop. Inherits from Piece.
 
-    def is_valid_move(self, new_position: tuple) -> bool:
+        Args:
+            colour (str): The colour of the bishop.
+            position (tuple): The starting position of the bishop.
+        """
+        super().__init__(colour, "Bishop", position)
+
+    def is_valid_move(self, new_position: tuple,
+                      chess_board: ChessBoard) -> bool:
         """
         Checks if a move is valid for the bishop.
 
         Args:
             new_position (tuple): The proposed new position for the bishop.
+            chess_board (ChessBoard): The current state of the board.
 
         Returns:
             bool: True if the move is valid, False otherwise.
         """
-        if not self.is_move_within_bounds(new_position):
-            return False
-
-        # Check if the move is valid
-        return self._is_bishop_move_valid(new_position)
+        return MoveValidator.is_bishop_move_valid(self.position, new_position,
+                                                  chess_board)
